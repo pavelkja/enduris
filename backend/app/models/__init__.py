54a25e59eee304076bc5d19715cec305e10d1a32
pavelkja <pavelkja@gmail.com>
2026-03-25 13:41:49 +0100

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,6 @@
+from .activity import Activity
+from .activity_metric import ActivityMetric
 from .activity_stream import ActivityStream
+from .user import User
+
+__all__ = ["Activity", "ActivityMetric", "ActivityStream", "User"]
