73e16e124a0b85ddc59426c0809994fdee9464db
pavelkja <pavelkja@gmail.com>
2026-03-27 13:32:13 +0100

    Update redis.py

--- backend/app/workers/redis.py
+++ backend/app/workers/redis.py
@@ -1,8 +1,13 @@
 import os
-
 from redis import Redis
 
+_redis_conn = None
 
 def get_redis_connection() -> Redis:
-    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    return Redis.from_url(redis_url)
+    global _redis_conn
+
+    if _redis_conn is None:
+        redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+        _redis_conn = Redis.from_url(redis_url)
+
+    return _redis_conn
