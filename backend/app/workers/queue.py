08a1a8332914199fe6d79bc11b2490f9a9ca72ad
pavelkja <pavelkja@gmail.com>
2026-03-27 13:32:43 +0100

    Update queue.py

--- backend/app/workers/queue.py
+++ backend/app/workers/queue.py
@@ -1,7 +1,12 @@
 from rq import Queue
-
 from app.workers.redis import get_redis_connection
 
+_queue = None
 
 def get_default_queue() -> Queue:
-    return Queue("default", connection=get_redis_connection())
+    global _queue
+
+    if _queue is None:
+        _queue = Queue("default", connection=get_redis_connection())
+
+    return _queue
