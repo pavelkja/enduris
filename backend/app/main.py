ca2aec0b1265c8c87444854ff8f4090de9c5d884
pavelkja <pavelkja@gmail.com>
2026-03-27 09:21:57 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,14 +8,17 @@ from app.models.activity_stream import ActivityStream
 from app.models.activity_metric import ActivityMetric
 
 from app.api.dashboard import router as dashboard_router
-
 from app.routers import health
+
+# 🔽 RQ importy
+from app.workers.queue_config import queue
+from app.workers.jobs import test_job
 
 
 app = FastAPI(
     title="Enduris API",
     version="0.1"
-)
+    )
 
 app.include_router(dashboard_router, prefix="/api")
 
@@ -29,3 +32,10 @@ app.include_router(health.router)
 @app.get("/")
 def root():
     return {"status": "Enduris backend running"}
+
+
+# 🔽 TEST ENDPOINT PRO RQ
+@app.get("/test-job")
+def run_test_job():
+    job = queue.enqueue(test_job)
+    return {"job_id": job.id}
